ed3ac5d6d87eb2f20168116b28993676dbf6b9b4
Wanderson Castro <wandersonce1@hotmail.com>
2023-07-31 10:09:22 -0700

    Creating the route group folders

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+function DashboardPage() {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default DashboardPage
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,6 @@
+
+export default function LadingPage() {
+  return (
+    <div>Lading Page</div>
+  )
+}
