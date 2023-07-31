2ca457962258d2ea4e739d6bf4de301e57afff4a
Wanderson Castro <wandersonce1@hotmail.com>
2023-07-31 11:58:19 -0700

    Adding Mobile Sidebar Menu

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -19,37 +19,37 @@ const routes =[
   {
     label: "Conversation",
     icon: MessageSquare,
-    href: "/dashboard",
+    href: "/conversation",
     color: "text-violet-500"
   },
   {
     label: "Image Generation",
     icon: ImageIcon,
-    href: "/dashboard",
+    href: "/image",
     color: "text-pink-700"
   },
   {
     label: "Video Generation",
     icon: VideoIcon,
-    href: "/dashboard",
+    href: "/video",
     color: "text-orange-700"
   },
   {
     label: "Music Generation",
     icon: Music,
-    href: "/dashboard",
+    href: "/music",
     color: "text-emerald-500"
   },
   {
     label: "Code Generation",
     icon: Code,
-    href: "/dashboard",
+    href: "/code",
     color: "text-green-700"
   },
   {
     label: "Settings",
     icon: Settings,
-    href: "/dashboard",
+    href: "/settings",
   },
 ]
 
