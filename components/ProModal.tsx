f806b91f0cc06c9bf95a623a9fb21ff4883056cf
Wanderson Castro <wandersonce1@hotmail.com>
2023-08-09 13:36:03 -0700

    Adjusting Routes inside middleware

--- components/ProModal.tsx
+++ components/ProModal.tsx
@@ -7,6 +7,8 @@ import {MessageSquare,ImageIcon,VideoIcon,Music,Code, Check, Zap} from "lucide-r
 import { Card } from "./ui/card";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
+import axios from "axios";
+import { useState } from "react";
 
 const tools = [
   {
@@ -44,6 +46,20 @@ const tools = [
 
 export default function ProModal() {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe= async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/stripe");
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log(error, "STRIPE_CLIENT_ERROR")
+    } finally{
+      setLoading(false);
+    }
+  }
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -74,7 +90,7 @@ export default function ProModal() {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button size="lg" variant="premium" className="w-full ">
+          <Button onClick={onSubscribe} size="lg" variant="premium" className="w-full ">
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white"/>
           </Button>
--- constants.ts
+++ constants.ts
@@ -1,1 +1,1 @@
-export const MAX_FREE_COUNTS = 3;
+export const MAX_FREE_COUNTS = 2;
