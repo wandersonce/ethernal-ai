a8c34ea030cf9ea5a949eb4f705849a81765e42a
Wanderson Castro <wandersonce1@hotmail.com>
2023-08-10 09:31:41 -0700

    Landing Navbar

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,22 +1,9 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { LandingNavbar } from "@/components/LadingNavbar";
 
 export default function LadingPage() {
   return (
-    <div>Lading Page
-      <div>
-        <Link href="/sign-in">
-        <Button>
-          Login
-        </Button>
-        </Link>
-        <Link href="/sign-up">
-        <Button>
-          Register
-        </Button>
-        </Link>
-      </div>
-
+    <div className="h-full">
+      <LandingNavbar />
     </div>
   )
 }
